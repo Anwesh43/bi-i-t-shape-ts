63c744102739da1d1bc0b7578decab2046784b96
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 16:04:47 +0530

    adding BITSNode class

--- index.ts
+++ index.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class BITSNode {
+
+    prev : BITSNode 
+    next : BITSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BITSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBITSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BITSNode {
+        var curr : BITSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
