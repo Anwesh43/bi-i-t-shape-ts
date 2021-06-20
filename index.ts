271987118592bdbd5714a2fb62953f061e994241
Anwesh43 <anweshthecool0@gmail.com>
2021-06-20 15:44:17 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,40 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiITShape(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+        context.rotate(deg * sc4)
+        DrawingUtil.drawLine(context, 0, -size * sc1, 0, size * sc1)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            const scj : number = ScaleUtil.divideScale(scale, j + 1, parts)
+            DrawingUtil.drawLine(context, 0, -size / 2, size * 0.5 * scj, -size / 2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawBITSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiITShape(context, scale)
+    }
+}
